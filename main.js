abe395b162437d3a384058083472b46e9b832b9c
yeojin Kim <jjiny2022@kakao.com>
2022-11-19 23:19:39 +0900

    .

---
+++ main.js
@@ -0,0 +1,40 @@
+//change navbar styles on scroll
+
+window.addEventListener('scroll',()=>{
+  document.querySelector('nav').classList.toggle(
+      'window-scroll', window.scrollY>0 )
+})
+
+//show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq=>{
+  faq.addEventListener('click',()=>{
+    faq.classList.toggle('open');
+
+    //change icon
+    const icon = faq.querySelector('.faq__icon i');
+    if(icon.className=== 'uil uil-plus'){
+      icon.className = "uil uil-minus"
+    } else{
+      icon.className="uil uil-plus";
+    }
+  })
+})
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView:1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  //when window width is >= 600px
+  breakpoints:{
+    600: {
+      slidesPerView:2
+    }
+  }
+});
